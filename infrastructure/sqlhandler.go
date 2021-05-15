8f6c25a800f5d2f098fb53f2836d8a82101e8420
yokoro13 <yokoro.kaito.yi3@is.naist.jp>
2021-05-16 03:11:09 +0900

    Change package configuration

--- infrastructure/sqlhandler.go
+++ infrastructure/sqlhandler.go
@@ -2,22 +2,22 @@ package infrastructure
 
 import (
 	"database/sql"
-	"sakata-shoten-common/interface/database/repository"
+	"sakata-shoten-common/interface/database"
 )
 
 type SqlHandler struct {
 	Conn *sql.DB
 }
 
-func (s2 SqlHandler) Execute(s string, i ...interface{}) (repository.Result, error) {
+func (s2 SqlHandler) Execute(s string, i ...interface{}) (database.Result, error) {
 	panic("implement me")
 }
 
-func (s2 SqlHandler) Query(s string, i ...interface{}) (repository.Row, error) {
+func (s2 SqlHandler) Query(s string, i ...interface{}) (database.Row, error) {
 	panic("implement me")
 }
 
-func NewSqlHandler() repository.SqlHandler {
+func NewSqlHandler() database.SqlHandler {
 	conn, err := sql.Open("mysql", "root:@tcp(db:3306)/sample")
 	if err != nil {
 		panic(err.Error)
--- interface/database/sql_handler.go
+++ interface/database/sql_handler.go
@@ -1,4 +1,4 @@
-package repository
+package database
 
 type SqlHandler interface {
 	Execute(string, ...interface{}) (Result, error)
