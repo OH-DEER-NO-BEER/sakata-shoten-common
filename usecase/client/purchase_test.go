8753751fe601c87cd02ecf16d74b7071274722f3
yokoro13 <yokoro.kaito.yi3@is.naist.jp>
2021-05-20 14:26:55 +0900

    Add test

--- usecase/client/purchase_test.go
+++ usecase/client/purchase_test.go
@@ -1,7 +1,22 @@
 package client
 
-import "testing"
+import (
+	"sakata-shoten-common/entity"
+	"testing"
+	"time"
+)
 
 func TestPurchase(t *testing.T) {
-
+	client := entity.Client{
+		ID:    0,
+		Name:  "test man",
+		Email: "a@com",
+		Debt:  100}
+	item := entity.Item{
+		Id:    0,
+		Name:  "test item",
+		Price: 100,
+		Time:  time.Now(),
+	}
+	purchase(client, item)
 }
